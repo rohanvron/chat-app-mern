74d51384a6b33ed579ca01454df878afea605325
rohanvron <164373071+rohanvron@users.noreply.github.com>
2024-06-27 18:04:03 +0530

    deploy-3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'frontend', // Set the root directory to 'frontend'
   base: '/chat-app-mern/', // Set the base path for the deployed site
   build: {
-    outDir: 'dist', // Set the output directory to 'dist'
+    outDir: 'dist', // Set the output directory to 'dist' within the 'frontend' directory
   },
 });
